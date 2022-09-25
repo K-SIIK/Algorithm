9eb289d94adaa6088355f5089c76782d2c187e77
KANG <wind_breath@naver.com>
2022-09-25 20:48:26 +0900

    Prob20

---
+++ src/Prob20.java
@@ -0,0 +1,18 @@
+// 문자열 내림차순으로 배치하기
+// 자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴
+
+import java.util.Arrays;
+import java.util.Collections;
+
+class Prob20 {
+    // 20 번 다시 풀기!
+    public String solution(String s) {
+        String answer = "";
+        String[] strings = s.split("");
+        Arrays.sort(strings, Collections.reverseOrder());
+        for (String string : strings) {
+            answer += string;
+        }
+        return answer;
+    }
+}
