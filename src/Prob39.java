cff600d73ba72117024ff05bf2bf1a05f8605129
KANG <wind_breath@naver.com>
2022-09-28 22:27:54 +0900

    solve

--- src/Prob39.java
+++ src/Prob39.java
@@ -1,13 +1,23 @@
 // 숫자의 표현
 // 자연수 n이 매개변수로 주어질 때, 연속된 자연수들로 n을 표현하는 방법의 수
 
-class Prob39 {
+class Solution { // 9점
     public int solution(int n) {
-        if (n == 1) return 1;
-        int answer = 1; // 자기 자신
-        if (n % 2 == 1) answer++;
-
-
+        int answer = 1;
+        int num = 1;
+        while (num <= (double) (n / 2)) {
+            int sum = 0;
+            for (int i = num; i <= n / 2 + 1; i++) {
+                sum += i;
+                if (sum == n) {
+                    answer++;
+                    break;
+                } else if (sum > n) {
+                    break;
+                }
+            }
+            num++;
+        }
         return answer;
     }
 }
